7f6d4b85f863f4aabf68f9b4dd78471c112364ad
ruijuanwang <1938608827@qq.com>
2020-03-17 14:41:07 +0800

    登录-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,6 +4,22 @@
     <!-- 导航 left-arrow是返回箭头-->
     <!-- @click-left 点击左侧事件  $router.back()=$router.go(-1)-->
     <van-nav-bar @click-left='$router.back()' title="登录模块" left-arrow></van-nav-bar>
+
+  <!-- 登录布局 -->
+   <!-- cell-group 作为容器来提供外边框 -->
+   <van-cell-group>
+     <!-- 手机号 -->
+      <van-field label="手机号" placeholder="手机号"></van-field >
+      <!-- 验证码 -->
+      <van-field label="验证码" placeholder="验证码">
+          <!-- 具名插槽 发送验证码 -->
+          <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field >
+    </van-cell-group>
+    <!-- 登录按钮 -->
+    <div style="margin: 16px;">
+    <van-button size="small" round block type="info">登录</van-button>
+    </div>
   </div>
 </template>
 
