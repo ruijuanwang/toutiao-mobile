e5f99fc82e4b01d0c34aa648d86ab066c1bfeb7b
ruijuanwang <1938608827@qq.com>
2020-03-19 19:10:24 +0800

    article-列表文章布局

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -7,18 +7,42 @@
   <div class="scroll-wrapper">
     <!-- 文章列表 组件 -->
     <!-- van-list组件 如果不干涉 初始化完毕 就会检测 自己距离底部的长度 如果超出了限定 就会执行 load事件 自动把绑定的loading 变成 true -->
-   <!-- 下拉刷新组件 包裹 列表组件 -->
+    <!-- 下拉刷新组件 包裹 列表组件 -->
     <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="successText">
       <van-list v-model="upLoading" :finished="finished" finished-text="没有了哦" @load="onLoad">
-          <!-- 循环内容 -->
-          <van-cell-group>
-             <!-- 循环单元格 -->
-             <van-cell v-for="item in articles" :key="item" title="胤娟" :value="'加油'+item"></van-cell>
-          </van-cell-group>
+        <!-- 循环内容 -->
+        <van-cell-group>
+          <!-- 循环单元格 -->
+          <van-cell v-for="item in articles" :key="item">
+            <!-- 放置元素 文章列表的循环项 无图 单图 三图 -->
+            <div class="article_item">
+              <!-- 标题 -->
+              <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+              <!-- 三图图片 -->
+              <div class="img_box">
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <!-- 单图模式  单图模式暂时隐藏-->
+              <!-- <div class="img_box">
+                <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div> -->
+
+              <!-- 作者信息 -->
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+                <span class="close">
+                  <van-icon name="cross"></van-icon>
+                </span>
+              </div>
+            </div>
+          </van-cell>
+        </van-cell-group>
       </van-list>
-
     </van-pull-refresh>
-
   </div>
 </template>
 
@@ -79,5 +103,45 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.article_item{
+  h3{
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box{
+    display: flex;
+    justify-content: space-between;
+    .w33{
+      width: 33%;
+      height: 90px;
+    }
+    .w100{
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box{
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span{
+      padding-right: 10px;
+      &.close{
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
