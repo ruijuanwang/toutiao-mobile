a6943a309b9475fb8b9a4c10c6e900f0e2fa08e2
ruijuanwang <1938608827@qq.com>
2020-03-21 12:43:24 +0800

    更多操作-举报文章-常量准备

--- src/views/home/components/more-action.vue
+++ src/views/home/components/more-action.vue
@@ -13,23 +13,18 @@
     </van-cell-group>
     <!-- 单元格组 2 -->
     <van-cell-group v-else>
-      <van-cell icon="arrow-left" @click="isReport=false">返回</van-cell>
-      <van-cell>侵权</van-cell>
-      <van-cell>色情</van-cell>
-      <van-cell>暴力</van-cell>
-      <van-cell>低俗</van-cell>
-      <van-cell>不适</van-cell>
-      <van-cell>错误</van-cell>
-      <van-cell>其他</van-cell>
+      <van-cell v-for="item in reports" :key="item.value" icon="arrow-left" @click="isReport=false">{{ item.label }}</van-cell>
     </van-cell-group>
   </div>
 </template>
 
 <script>
+import { reports } from '@/api/constants' // 引入常量变量
 export default {
   data () {
     return {
-      isReport: false
+      isReport: false, // 是否举报的意思
+      reports // 常量数组 相当于定义了一个reports变量 初始值来源于 constants中的reports变量
     }
   }
 }
