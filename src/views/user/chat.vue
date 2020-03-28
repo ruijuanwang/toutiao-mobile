8fe758716ae0b4f6d51362170407fa36ba557911
ruijuanwang <1938608827@qq.com>
2020-03-28 20:46:33 +0800

    个人中心-小智同学-实现滚动条和关闭连接

--- src/views/user/chat.vue
+++ src/views/user/chat.vue
@@ -2,7 +2,7 @@
   <!-- 小智同学 -->
  <div class="container">
     <van-nav-bar fixed left-arrow @click-left="$router.back()" title="小智同学"></van-nav-bar>
-    <div class="chat-list">
+    <div class="chat-list" ref="myList">
       <!-- 把list消息循环渲染到视图上 -->
       <!-- 小智同学说的话在左边 我说的话在右边 -->
       <!-- name : xz =>小智 左边 name !=xz =>我 =>右边 -->
@@ -31,6 +31,7 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import XZImg from '@/assets/xz.jpg' // 引入小智头像
 import { mapState } from 'vuex' // 用户头像
 import io from 'socket.io-client'// 引入socke.io的客户端
@@ -68,9 +69,27 @@ export default {
     this.socket.on('message', data => {
       // data就是 客户服务器 回复的消息 接收的消息都是小智回复的
       this.list.push({ ...data, name: 'xz' }) // 追加聊天记录
+      this.scrollBottom() // 收到消息后 调用滚动条方法
     })
   },
   methods: {
+    // 滚动条滚动到最底部
+    scrollBottom () {
+      // vuejs 数据变化=> 异步的 => 视图更新
+      // vuejs 内部的渲染 是异步的 不是同步的
+      // 我们需要等到 异步更新 完毕之后 才去执行滚动
+      // this.$nextTick 次函数 会在上一次数据 更新 并且完成 页面渲染之后执行
+      // 第1种方式 组件实例方式
+      // this.$nextTick(() => {
+      //   // 在这个回调函数中 上一次的数据 更新结果 已经 反映到了视图上 视图已经更新了
+      //   this.$refs.myList.scrollTop = this.$refs.myList.scrollHeight
+      //   // 此时表示 滚动条滚动的时候 视图上数据已经是最新的了
+      // })
+      // 第2种方式 全局对象的方式 也表示上一次数据更新 完成页面渲染之后执行该函数
+      Vue.nextTick(() => {
+        this.$refs.myList.scrollTop = this.$refs.myList.scrollHeight // 将滚动条滚动到最底部
+      })
+    },
     // 发送消息
     async send () {
       // 调用websocket 发出一个消息
@@ -90,7 +109,14 @@ export default {
       // 发完消息之后
       this.value = '' // 清空输入框
       this.loading = false// 关闭状态
+      this.scrollBottom() // 发消息后 调用滚动条方法
     }
+  },
+  // 钩子函数  组件实例销毁前
+  beforeDestroy () {
+    // 需要在实例销毁前 把websocket 连接给关掉
+    // ws.close() 原生   socket.io = > close
+    this.socket.close() // 关闭连接
   }
 
 }
